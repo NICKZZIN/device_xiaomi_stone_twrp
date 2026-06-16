ad5ea09abd7969b99113f9854b4d46722443fda3
NICKZZIN <elmitoso190@hotmail.com>
2026-06-16 18:39:09 -0300

    Atualizando a árvore para a base unificada Gemstone

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_gemstone.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_gemstone-user \
+    omni_gemstone-userdebug \
+    omni_gemstone-eng
