8063a31734bde24a11fb1191b8e80f3450055e22
NICKZZIN <76818604+NICKZZIN@users.noreply.github.com>
2026-06-17 04:31:35 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,27 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Configure base.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Configure core_64_bit_only.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Configure gsi_keys.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Configure Virtual A/B (Essencial para o Android 12+)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
+# Configure twrp
+$(call inherit-product, vendor/twrp/config/common.mk)
+
 LOCAL_PATH := device/xiaomi/gemstone
+
+# API Levels
+PRODUCT_SHIPPING_API_LEVEL := 30
+PRODUCT_TARGET_VNDK_VERSION := 31
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -15,21 +35,50 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.1-impl-qti.recovery \
+    bootctrl.holi.recovery
 
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Twrp Decryption (Para ler seus arquivos)
 PRODUCT_PACKAGES += \
-    bootctrl.holi
+    qcom_decrypt \
+    qcom_decrypt_fbe
 
-# PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-#     bootctrl.holi \
-#     libgptutils \
-#     libz \
-#     libcutils
+# Fastbootd (Para comandos fastboot dentro do TWRP)
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
 
+# Update engine
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
     update_engine \
-    update_verifier \
-    update_engine_sideload
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+# Soong Namespaces : Qcom commonsys Display
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/commonsys-intf/display \
+    $(LOCAL_PATH)
+
+# Drivers de Tela (A cereja do bolo pra evitar tela preta)
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libdisplayconfig.qti \
+    libion \
+    vendor.display.config@1.0 \
+    vendor.display.config@2.0
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
